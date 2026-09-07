69bbbddc9fd11f0cb147c78628e4c691972e6a17
Ajit Nandi <nandiajito2@gmail.com>
2026-09-07 23:04:21 +0530

    Allow production frontend in CORS

--- server/src/app.js
+++ server/src/app.js
@@ -14,7 +14,30 @@ const app = express();
 
 app.use(
     cors({
-        origin: /^http:\/\/localhost:517[3-9]$/,
+        origin: [
+            "http://localhost:5173",
+            "http://localhost:5174",
+            "http://localhost:5175",
+            "http://localhost:5176",
+            "http://localhost:5177",
+            "http://localhost:5178",
+            "http://localhost:5179",
+            "https://saasflow-gamma.vercel.app",
+        ],
+        credentials: true,
+    })
+); app.use(
+    cors({
+        origin: [
+            "http://localhost:5173",
+            "http://localhost:5174",
+            "http://localhost:5175",
+            "http://localhost:5176",
+            "http://localhost:5177",
+            "http://localhost:5178",
+            "http://localhost:5179",
+            "https://saasflow-gamma.vercel.app",
+        ],
         credentials: true,
     })
 );
